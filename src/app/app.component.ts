35d67737246d57917c42ddccdf10787cf7cad55d
radia idelkadi <idelkadiradia@gmail.com>
2025-02-22 15:03:49 +0100

    add sidebar component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { SidebarComponent } from './components/shared/sidebar/sidebar.component';
+import { HeaderComponent } from './components/shared/header/header.component';
 
 @Component({
   selector: 'app-root',
-  imports: [],
+  imports: [RouterOutlet , SidebarComponent , HeaderComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'service_codingArt';
+  email= "codingArt@gmail.com"
 }
